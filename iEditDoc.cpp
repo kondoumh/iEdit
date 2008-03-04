9db57ac33476e1d5182fba0710c8c75d7ea23397
kondoh <kondoh@62cbcccb-ddce-9d43-9cae-9d8066a2bb0e>
2008-03-04 15:49:20 +0000

    新規ファイルの、ルートノードのラベルからファイル名を生成する際の、文字化け対策を施した
    git-svn-id: http://subversion.assembla.com/svn/iedit_devel/trunk/iEdit@63 62cbcccb-ddce-9d43-9cae-9d8066a2bb0e

--- iEditDoc.cpp
+++ iEditDoc.cpp
@@ -3907,13 +3907,18 @@ void iEditDoc::OnFileSaveAs()
 CString iEditDoc::getSafeFileName(const CString& str) const
 {
 	CString rs;
-	
 	for (int i = 0; i < str.GetLength(); i++) {
-		char a = str.GetAt(i);
+		TCHAR a = str.GetAt(i);
+		if ((a == '|' || a == '\\') && i > 0) {
+			if (_ismbblead(str.GetAt(i-1))) {
+				rs += _T(a);
+				continue;
+			}
+		}
 		if (a == '\n' || a == '\r') continue;
 		UINT type = ::PathGetCharType(a);
 		if (type == GCT_INVALID || type & GCT_WILD || type & GCT_SEPARATOR) continue;
-		rs += a;
+		rs += _T(a);
 	}
 	return rs;
 }
