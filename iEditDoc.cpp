d7c7989e870a2fd2228010f9d23223fffc977cc2
kondoumh <kondoumh@62cbcccb-ddce-9d43-9cae-9d8066a2bb0e>
2009-04-23 12:42:24 +0000

    正規表現 uri をリンクに変換 refs #21
    git-svn-id: http://subversion.assembla.com/svn/iedit_devel/trunk/iEdit@200 62cbcccb-ddce-9d43-9cae-9d8066a2bb0e

--- iEditDoc.cpp
+++ iEditDoc.cpp
@@ -3572,7 +3572,7 @@ CString iEditDoc::procWikiNotation(const CString &text)
 	const std::tr1::regex h3("^\\*\\*([^\\*].*)$");
 	const std::tr1::regex l1("^-([^-].*)$");
 	const std::tr1::regex l2("^--([^-].*)");
-	//const std::tr1::regex uri("(https?:\\/\\/[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#]+");
+	const std::tr1::regex uri("^.*(https?://[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#]+).*$");
 	std::tr1::match_results<std::string::const_iterator> result;
 	int pos = 0;
 	CString rtnStr;
@@ -3597,13 +3597,12 @@ CString iEditDoc::procWikiNotation(const CString &text)
 			rtnStr += result[1].str().c_str();
 			rtnStr += "</li>\n";
 			rtnStr += "</ul>\n";
-		//} else if (std::tr1::regex_match(line, result, uri)) {
-		//	DEBUG_WRITE(line.c_str());
-		//	rtnStr += "<a href=\"";
-		//	rtnStr += result[0].str().c_str();
-		//	rtnStr += "\"";
-		//	rtnStr += result[0].str().c_str();
-		//	rtnStr += "</a>\n";
+		} else if (std::tr1::regex_match(line, result, uri)) {
+			rtnStr += "<a href=\"";
+			rtnStr += result[1].str().c_str();
+			rtnStr += "\" target=\"_blank\">";
+			rtnStr += result[1].str().c_str();
+			rtnStr += "</a><br />\n";
 		} else {
 			rtnStr += line.c_str();
 			rtnStr += "<br />\n";
