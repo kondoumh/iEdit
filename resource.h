179863a1eb4fab01c1c1e817fdffff646a68bff1
kondoumh <kondoumh@62cbcccb-ddce-9d43-9cae-9d8066a2bb0e>
2009-11-07 16:25:15 +0000

    アイコンもお色直し
    git-svn-id: http://subversion.assembla.com/svn/iedit_devel/trunk/iEdit@231 62cbcccb-ddce-9d43-9cae-9d8066a2bb0e

--- resource.h
+++ resource.h
@@ -510,7 +510,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        213
+#define _APS_NEXT_RESOURCE_VALUE        214
 #define _APS_NEXT_COMMAND_VALUE         33083
 #define _APS_NEXT_CONTROL_VALUE         1122
 #define _APS_NEXT_SYMED_VALUE           105
