f6bf3bf7ed5f12ff4161d45521d382cb4b07790d
kondoumh <kondoumh@62cbcccb-ddce-9d43-9cae-9d8066a2bb0e>
2012-09-30 11:31:31 +0000

    XML エクスポートのスタイルシートはデフォルトでは指定しないようにした。
    git-svn-id: http://subversion.assembla.com/svn/iedit_devel/trunk/iEdit@364 62cbcccb-ddce-9d43-9cae-9d8066a2bb0e

--- iEdit.cpp
+++ iEdit.cpp
@@ -445,7 +445,7 @@ void CiEditApp::OnFileNew()
 
 void CiEditApp::getOtherProfile()
 {
-	m_rgsOther.bSetStylesheet = AfxGetApp()->GetProfileInt(REGS_OTHER, _T("XML StyleSheet"), TRUE);
+	m_rgsOther.bSetStylesheet = AfxGetApp()->GetProfileInt(REGS_OTHER, _T("XML StyleSheet"), FALSE);
 	m_rgsOther.strStyleSheetFile = AfxGetApp()->GetProfileString(REGS_OTHER, _T("XML StyleSheet Name"), _T("iedit.xsl"));
 	m_rgsOther.bOpenFilesAfterExport = AfxGetApp()->GetProfileInt(REGS_OTHER, _T("Open Files After Export"), TRUE);
 	m_rgsOther.bOutputFileLinksOnExport = AfxGetApp()->GetProfileInt(REGS_OTHER, _T("Output Filelinks On Export"), FALSE);
