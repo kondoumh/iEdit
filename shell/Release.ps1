3604cd2c871ac28d2fedb9f7ba083540d0d0cb6d
kondoumh <kondoumh@gmail.com>
2018-02-14 00:53:17 +0900

    complete release shell.

--- shell/Release.ps1
+++ shell/Release.ps1
@@ -1,7 +1,26 @@
 $ReleaseDir = "..\Release"
-$ReleaseModule = $ReleaseDir + "\iEdit.exe";
+$ReleaseModule = "$ReleaseDir\iEdit.exe";
+$DocDir = "..\doc"
 
-$arr = ([System.Diagnostics.FileVersionInfo]::GetVersionInfo($ReleaseModule).FileVersion).Split('.') | Select-Object -First 3
-$ReleaseVersion = [string]::Join("", $arr)
+$fileVersion = [Diagnostics.FileVersionInfo]::GetVersionInfo($ReleaseModule).FileVersion
+$releaseVersion = [string]::Join("", ($fileVersion.Split('.') | Select-Object -First 3))
+$archiveFile = "$ReleaseDir\iedit$releaseVersion.zip"
 
-echo $ReleaseVersion
+echo "File Version: $fileVersion"
+echo $archiveFile
+
+Compress-Archive -Path $ReleaseModule -DestinationPath $archiveFile -Force
+Copy-Item -Path "..\README.md" -Destination "$ReleaseDir\README.txt"
+Compress-Archive -Path "$ReleaseDir\README.txt" -DestinationPath $archiveFile -Update
+Compress-Archive -Path "$DocDir\iEditDoc.iedx" -DestinationPath $archiveFile -Update
+Compress-Archive -Path "$DocDir\albatross.ies" -DestinationPath $archiveFile -Update
+Compress-Archive -Path "$DocDir\ui\" -DestinationPath $archiveFile -Update
+
+$conf = Get-Content "$ReleaseDir\upload.conf" -Raw | ConvertFrom-StringData
+$client = New-Object System.Net.WebClient
+$client.Credentials = New-Object System.Net.NetworkCredential($conf.Item("user"), $conf.Item("pass"))
+$hostname = $conf.Item("hostname")
+$remotePath = $conf.Item("remotedir") + "iedit$releaseVersion.zip"
+$client.BaseAddress = "ftp://" + $conf.Item("hostname") + $conf.Item("remoteroot") 
+$client.UploadFile($remotePath, $archiveFile)
+$client.Dispose()
