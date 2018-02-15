2dd4d81700f5dd01af3908a0d49af9b74f8ba1c4
KONDO Masahiro <kondoumh@gmail.com>
2018-02-15 12:50:22 +0900

    fix warnings.

--- shell/Release.ps1
+++ shell/Release.ps1
@@ -6,8 +6,8 @@ $fileVersion = [Diagnostics.FileVersionInfo]::GetVersionInfo($ReleaseModule).Fil
 $releaseVersion = [string]::Join("", ($fileVersion.Split('.') | Select-Object -First 3))
 $archiveFile = "$ReleaseDir\iedit$releaseVersion.zip"
 
-echo "File Version: $fileVersion"
-echo $archiveFile
+Write-Output "File Version: $fileVersion"
+Write-Output $archiveFile
 
 Compress-Archive -Path $ReleaseModule -DestinationPath $archiveFile -Force
 Copy-Item -Path "..\README.md" -Destination "$ReleaseDir\README.txt"
@@ -19,7 +19,6 @@ Compress-Archive -Path "$DocDir\ui\" -DestinationPath $archiveFile -Update
 $conf = Get-Content "$ReleaseDir\upload.conf" -Raw | ConvertFrom-StringData
 $client = New-Object System.Net.WebClient
 $client.Credentials = New-Object System.Net.NetworkCredential($conf.Item("user"), $conf.Item("pass"))
-$hostname = $conf.Item("hostname")
 $remotePath = $conf.Item("remotedir") + "iedit$releaseVersion.zip"
 $client.BaseAddress = "ftp://" + $conf.Item("hostname") + $conf.Item("remoteroot") 
 $client.UploadFile($remotePath, $archiveFile)
