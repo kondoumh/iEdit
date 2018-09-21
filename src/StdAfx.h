34b36de540a2e68f277dcd6e591efb002b03d5b4
kondoumh <kondoumh@gmail.com>
2018-09-22 05:35:26 +0900

    update StdAfx.h

--- src/StdAfx.h
+++ src/StdAfx.h
@@ -1,32 +1,17 @@
 ﻿// stdafx.h : 標準のシステム インクルード ファイル、
 //            または参照回数が多く、かつあまり変更されない
 //            プロジェクト専用のインクルード ファイルを記述します。
-//
 
-#if !defined(AFX_STDAFX_H__96DFF9B9_1881_11D3_808A_00A0C9B72FDD__INCLUDED_)
-#define AFX_STDAFX_H__96DFF9B9_1881_11D3_808A_00A0C9B72FDD__INCLUDED_
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
 
-#define VC_EXTRALEAN		// Windows ヘッダーから殆ど使用されないスタッフを除外します。
-
-
-#ifndef WINVER				// Windows XP 以降のバージョンに固有の機能の使用を許可します。
-#define WINVER 0x0501		// これを Windows の他のバージョン向けに適切な値に変更してください。
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN        // Windows ヘッダーから使用されていない部分を除外します。
 #endif
 
-#ifndef _WIN32_WINNT		// Windows XP 以降のバージョンに固有の機能の使用を許可します。                   
-#define _WIN32_WINNT 0x0501	// これを Windows の他のバージョン向けに適切な値に変更してください。
-#endif						
+#include <SDKDDKVer.h>
 
-#ifndef _WIN32_WINDOWS		// Windows 98 以降のバージョンに固有の機能の使用を許可します。
-#define _WIN32_WINDOWS 0x0410 // これを Windows Me またはそれ以降のバージョン向けに適切な値に変更してください。
-#endif
-
-#ifndef _WIN32_IE			// IE 6.0 以降のバージョンに固有の機能の使用を許可します。
-#define _WIN32_IE 0x0600	// これを IE の他のバージョン向けに適切な値に変更してください。
+#ifndef _WIN32_WINNT        // Windows 7 以降のバージョンに固有の機能の使用を許可します。
+#define _WIN32_WINNT 0x0601	
 #endif
 
 #include <afxwin.h>         // MFC のコアおよび標準コンポーネント
@@ -35,13 +20,15 @@
 #include <afxdisp.h>        // MFC のオートメーション クラス
 
 #ifndef _AFX_NO_OLE_SUPPORT
-#include <afxdtctl.h>		// MFC の Internet Explorer 4 コモン コントロール サポート
+#include <afxdtctl.h>       // MFC の Internet Explorer 4 コモン コントロール サポート
 #endif
 #ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>			// MFC の Windows コモン コントロール サポート
+#include <afxcmn.h>         // MFC の Windows コモン コントロール サポート
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
-#include <afxole.h>         // OLEサポート LinkViewのDｎD機能で仕様
+#include <afxole.h>         // OLE サポート LinkView の DnD 機能で使用
+
+#include <afxcontrolbars.h>
 
 #ifdef _UNICODE
 #if defined _M_IX86
@@ -55,14 +42,17 @@
 #endif
 #endif
 
+/////////////////////////////////// Standard Library ///////////////////////////////////
+
 #include <vector>
 #include <list>
 #include <map>
 #include <functional>
 #include <set>
 #include <stack>
-#include <afxcontrolbars.h>
 using namespace std;
+
+/////////////////////////////////// Data Structure /////////////////////////////////////
 
 struct LinkProps {
 	DWORD key;
@@ -119,7 +109,3 @@ typedef vector<LinkProps> LinkPropsVec;
 typedef set<DWORD> NodeKeySet;
 typedef vector<DWORD> NodeKeyVec;
 typedef map<DWORD, DWORD> NodeKeyMap;
-
-//{{AFX_INSERT_LOCATION}}
-
-#endif // !defined(AFX_STDAFX_H__96DFF9B9_1881_11D3_808A_00A0C9B72FDD__INCLUDED_)
